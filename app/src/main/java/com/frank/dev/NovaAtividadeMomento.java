027ae0b8f5138b7c593542ff2784ecb183d41342
frank <frankwco@gmail.com>
2020-06-06 18:29:16 -0300

    Melhorias Layout

--- app/src/main/java/com/frank/dev/NovaAtividadeMomento.java
+++ app/src/main/java/com/frank/dev/NovaAtividadeMomento.java
@@ -3,6 +3,8 @@ package com.frank.dev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class NovaAtividadeMomento extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class NovaAtividadeMomento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_atividade_momento);
+    }
+
+    public void testeChamada(View view){
+        Toast.makeText(this,"asdasd", Toast.LENGTH_SHORT).show();
     }
 }
